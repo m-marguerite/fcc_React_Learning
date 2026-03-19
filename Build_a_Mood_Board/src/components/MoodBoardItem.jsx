864e5c8363f15f54b330773d457ee709bf228649
Mickaël <74702735+m-marguerite@users.noreply.github.com>
2026-03-19 18:48:58 +0400

    Delete Build_a_Mood_Board/src directory

--- Build_a_Mood_Board/src/components/MoodBoardItem.jsx
+++
@@ -1,9 +0,0 @@
-export const MoodBoardItem = ({ color, image, description }) => {
-  return (
-    <div className="mood-board-item" style={{ backgroundColor: color }}>
-      {" "}
-      <img className="mood-board-image" src={image} />
-      <h3 className="mood-board-text">{description}</h3>
-    </div>
-  );
-};
